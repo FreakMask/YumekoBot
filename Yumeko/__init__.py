876c691759c943219a13bd0406a91184f3181332
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-02-28 22:18:53 +0530

    Update __init__.py

--- Yumeko/__init__.py
+++ Yumeko/__init__.py
@@ -43,23 +43,23 @@ if ENV:
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
-        ZODIAC = set(int(x) for x in os.environ.get("ZODIAC", "").split())
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
         DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        ZODYCK = set(int(x) for x in os.environ.get("DEMONS", "").split())
+        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        BOUNTY_HUNTER = set(int(x) for x in os.environ.get("HUNTER", "").split())
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     try:
-        HUNTER = set(int(x) for x in os.environ.get("BOUNTY HUNTER", "").split())
+        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
@@ -107,7 +107,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from yumeko.config import Development as Config
+    from Yumeko.config import Development as Config
 
     TOKEN = Config.TOKEN
 
